64cf64770792757a787d2d9c034813f34942c9df
Irakoze Ritha Theonestine <irakozeritha20@gmail.comclear>
2025-10-19 21:49:20 +0200

    configuration of tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className="bg-amber-200">This my first react app</h1>
+    </>
+  );
+}
+
+export default App;
