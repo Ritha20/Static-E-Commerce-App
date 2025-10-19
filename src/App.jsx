d90baf34e8131eb397da75c553e9cb0200e3dbf7
Irakoze Ritha Theonestine <irakozeritha20@gmail.comclear>
2025-10-19 22:29:25 +0200

    updating my pages to navigate using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import ProductCard from './components/ProductCard';
 import Footer from './components/Footer';
-
+import Home from './pages/Home';
+import Products from './pages/Products';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
       <div className="App min-h-screen flex flex-col">
         <Navbar />
-        <HeroSection />
-        <ProductCard />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
         <Footer />
-        
       </div>
     </Router>
   );
